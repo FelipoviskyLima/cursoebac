9105b94f2023a60799d4aa902f2ac39071b16f1a
Felipe Lima <filipovisky.lima@gmail.com>
2024-10-22 11:47:21 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    
+    $('#task-form').submit(function(event) {
+      event.preventDefault();
+      
+      var task = $('#task-input').val();
+  
+      if (task.trim() !== '') {
+        $('#task-list').append('<li>' + task + '</li>');
+        $('#task-input').val('');
+      }
+    });
+  
+    $('#task-list').on('click', 'li', function() {
+      $(this).toggleClass('completed');
+    });
+  });
+  
